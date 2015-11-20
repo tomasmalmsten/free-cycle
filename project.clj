5504c49bc017ac7a7d720160f461b0602626dc5e
Tomas Malmsten <tmalmsten@efolder.net>
2015-11-20 15:45:47 +0100

    Still not able to submit form, something with anti-forgery token that I've not managed to crack yet

--- project.clj
+++ project.clj
@@ -6,8 +6,10 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
+                 [ring "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [ring/ring-anti-forgery "1.0.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler free-cycle.handlers/app}
   :main ^:skip-aot free-cycle.core
--- src/free_cycle/handlers.clj
+++ src/free_cycle/handlers.clj
@@ -2,19 +2,56 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [hiccup.core :refer :all]))
-
-(defroutes app-routes
-  (GET "/" root-page)
-  (route/not-found "Not here mate, some were else."))
+            [ring.middleware.session :refer [wrap-session]]
+            [ring.middleware.anti-forgery :refer :all]
+            [ring.middleware.session.cookie :refer (cookie-store)]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]
+            [hiccup.form :refer :all]))
 
 (defn root-page []
   (html
    [:h1 "Free-cycle"]
    [:p "Hello there, this is free-cycling. Here you can give stuff away to other that needs it."]
    [:p "What do you want to do?" [:br] 
-    [:a [:href "products/add"] "Add product to give away"] [:br]
-    [:a [:href "products/search"] "Find product"]]))
+    [:a {:href "/products/add"} "Add product to give away"] [:br]
+    [:a {:href "/products/search"} "Find product"]]))
+
+(defn add-product-form []
+  (form-to [:post "/products/add"]
+           (label :title "Title") (text-field :title) [:br]
+           (label :description "Description") (text-area :description)[:br]
+           (label :upload-picture "Upload picture") (file-upload :upload-picture)[:br]
+           (label :giver-name "Your name") (text-field :givers-name)[:br]
+           (label :giver-email "Your e-mail") (email-field :givers-email)[:br]
+           (label :givers-phone "Your phone number") (text-field :givers-phone)[:br]
+           (label :givers-address-one "Your addres, line one") (text-field :givers-address-one)[:br]
+           (label :giver-address-two "Your address, line two") (text-field :givers-address-two)[:br]
+           (label :givers-address-three "Your addres, line three") (text-field :givers-address-trhee)[:br]
+           (label :givers-postcode "Your post code") (text-field :givers-postcode)[:br]
+           (label :givers-city "Your city") (text-field :givers-city)[:br]
+           (label :givers-country "Your country") (text-field :givers-country)[:br]
+           (submit-button "Save item")))
+
+(defn add-product-html []
+  (html5
+   {:lang "en"}
+   [:h1 "This is where we add something..."]
+   [:div
+    [:p "Complete the form and press save and you've just posted something"]
+    (add-product-form)]))
+
+(defn add-product [request]
+  (str request))
+
+(defroutes app-routes
+  (GET "/" [] (root-page))
+  (GET "/products/add" [] (add-product-html))
+  (POST "/products/add" request (add-product request))
+  (route/not-found "This part's probably not done yet. Perhaps it's not s'posed to every get done."))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults site-defaults)
+      (wrap-session {:cookie-attrs {:max-age 3600}
+                     :store (cookie-store {:key "not-currenlty-th"})})))
