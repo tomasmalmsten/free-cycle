f5fa96d347068629c133223ecd840504264add35
Tomas Malmsten <tmalmsten@efolder.net>
2015-11-20 10:01:11 +0100

    Update licens terms in project.clj, missed it in initial commit

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject free-cycle "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public Licens 3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot free-cycle.core
   :target-path "target/%s"
